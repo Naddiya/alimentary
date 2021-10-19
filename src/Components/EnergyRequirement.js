29e406dedaf18423ae3c50acbc426dbb33b598a1
Naddiya <nadiamedkouri@gmail.com>
2021-10-19 13:40:02 +0200

    refactor(energyrequirement): remove surperfluous code

--- src/Components/EnergyRequirement.js
+++ src/Components/EnergyRequirement.js
@@ -3,11 +3,8 @@ import { activity, gender, sport } from '../../data/data';
 import { calculateEnergyRequirement } from './utils';
 
 const EnergyRequirement = () => {
-  const [checked, setChecked] = useState(inputCheckboxes);
-  const [inputCheckboxes, setInputCheckboxes] = useState();
-  const [clear, setClear] = useState(false);
   const [needs, setNeeds] = useState(0);
-  const [targetValue, setTargetValue] = useState({});
+  const [targetValues, setTargetValues] = useState({});
 
   const activityCheckBoxes = activity.map(elem => ({
     name: elem.level,
@@ -23,43 +20,38 @@ const EnergyRequirement = () => {
   }));
 
   function handleCheck(e) {
-    const name = e.target.name;
-    const value = e.target.value;
-    const isChecked = e.target.checked;
     const boxes = document.getElementsByClassName(e.target.className);
     for (let i = 0; i < boxes.length; i++) {
       if (boxes[i].id !== e.target.id) {
         boxes[i].checked = false;
       }
     }
-    setInputCheckboxes(values => ({ ...values, [name]: value , isChecked}));
   };
 
   function handleChange(e) {
     handleCheck(e);
     const value = e.target.value;
     const name = e.target.name;
-    setTargetValue(values => ({ ...values, [name]: value }));
+    setTargetValues(values => ({ ...values, [name]: value }));
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    setNeeds(calculateEnergyRequirement(targetValue));
+    const result = calculateEnergyRequirement(targetValues)
+    !!result ? setNeeds(result) : setNeeds("Error make sure all fields are filled")
   };
 
   function handleClear(e) {
     e.preventDefault();
-    document.getElementById('calory-needs').reset()
-    setNeeds(0)
-    setClear(!clear);
-    console.log("energy requirement form cleared");
+    document.getElementById('calory-needs').reset();
+    setNeeds(0);
+    setTargetValues({...0});
   };
 
   return (
     <div className="energy">
       <h2>Calcul des dépenses energétiques</h2>
       <form id="calory-needs" onSubmit={handleSubmit}>
-
         <label htmlFor="height">Taille:</label>
         <input
           type="number"
@@ -78,7 +70,6 @@ const EnergyRequirement = () => {
           name="age"
           onChange={handleChange}
         />
-
         <h4>Dépenses énergétique de base</h4>
         <div className="activity">
           {activityCheckBoxes.map(elem =>
@@ -89,14 +80,13 @@ const EnergyRequirement = () => {
                 name="activityLevel"
                 id={elem.name}
                 value={elem.value}
-                defaultChecked={checked}
+                defaultChecked={false}
                 onChange={handleChange}
               />
               <label htmlFor={elem.name}>{elem.name}</label>
             </div>
           )}
         </div>
-
         <h4>Dépenses selon la pratique sportive</h4>
         <div className="sport">
           {sportCheckBoxes.map(elem =>
@@ -107,14 +97,13 @@ const EnergyRequirement = () => {
                 name="sportLevel"
                 id={elem.name}
                 value={elem.value}
-                defaultChecked={checked}
+                defaultChecked={false}
                 onChange={handleChange}
               />
               <label htmlFor={elem.name}>{elem.name}</label>
             </div>
           )}
         </div>
-
         <div className="sport-time">
           <label htmlFor="sportTime">Hours per week:</label>
           <input
@@ -123,7 +112,6 @@ const EnergyRequirement = () => {
             onChange={handleChange}
           />
         </div>
-
         <h4>Dépenses selon le genre</h4>
         <div className="gender">
           {genderCheckBoxes.map(elem =>
@@ -134,21 +122,18 @@ const EnergyRequirement = () => {
                 name="genderLevel"
                 value={elem.value}
                 id={elem.name}
-                defaultChecked={checked}
+                defaultChecked={false}
                 onChange={handleChange}
               />
               <label htmlFor={elem.name}>{elem.name}</label>
             </div>
           )}
         </div>
-
         <div className="calculation-result">Resultat : {needs}</div>
-
         <div className="call-to-action">
           <button className="button-calculate" type="submit" >Calculer</button>
           <button className="button-clear" onClick={handleClear}>Initialiser</button>
         </div>
-
       </form>
     </div>
   );
