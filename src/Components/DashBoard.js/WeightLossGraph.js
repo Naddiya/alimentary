6d98bc126a059cce384f0d6d00b7b1794fae2e17
Naddiya <nadiamedkouri@gmail.com>
2021-11-03 11:29:15 +0100

    feat: styled energyrequirement

--- src/Components/DashBoard.js/WeightLossGraph.js
+++ src/Components/DashBoard.js/WeightLossGraph.js
@@ -6,7 +6,6 @@ const loss = [];
 for (let i = 0; i < 8; i++) {
   loss.push(i);
 }
-console.log(loss);
 
 const data = {
   labels: ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13'],
--- src/Components/EnergyRequirement/index.js
+++ src/Components/EnergyRequirement/index.js
@@ -1,141 +1,177 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { activity, gender, sport } from '../../../data/data';
 import { calculateEnergyRequirement } from './calculations';
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  Card,
+  Paper,
+  OutlinedInput,
+  InputAdornment,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+} from '@mui/material';
 
 const EnergyRequirement = () => {
   const [needs, setNeeds] = useState(0);
-  const [targetValues, setTargetValues] = useState({});
+  const [clear, setClear] = useState(false);
+  const [values, setValues] = useState({
+    height: '',
+    weight: '',
+    age: '',
+    activityLevel: '',
+    sportLevel: '',
+    sportTime: '',
+    genderLevel: ''
+  });
 
-  const activityCheckBoxes = activity.map(elem => ({
+  const activityRadios = activity.map(elem => ({
     name: elem.level,
     value: elem.ratio,
   }));
-  const genderCheckBoxes = gender.map(elem => ({
+  const genderRadios = gender.map(elem => ({
     name: elem.type,
     value: elem.ratio,
   }));
-  const sportCheckBoxes = sport.map(elem => ({
+  const sportRadios = sport.map(elem => ({
     name: elem.level,
     value: elem.ratio,
   }));
 
-  function handleCheck(e) {
-    const boxes = document.getElementsByClassName(e.target.className);
-    for (let i = 0; i < boxes.length; i++) {
-      if (boxes[i].id !== e.target.id) {
-        boxes[i].checked = false;
-      }
+  const formRef = useRef(null);
+
+  const handleClear = () => {
+    setNeeds(0);
+    formRef.current.reset();
+    setValues({
+      height: '',
+      weight: '',
+      age: '',
+      activityLevel: '',
+      sportLevel: '',
+      sportTime: '',
+      genderLevel: ''
+    });
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const result = calculateEnergyRequirement(values);
+    if (!!result) {
+      setNeeds(result);
     }
   };
 
-  function handleChange(e) {
-    handleCheck(e);
-    const value = e.target.value;
-    const name = e.target.name;
-    setTargetValues(values => ({ ...values, [name]: value }));
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const result = calculateEnergyRequirement(targetValues)
-    !!result ? setNeeds(result) : setNeeds("Error make sure all fields are filled")
-  };
-
-  function handleClear(e) {
-    e.preventDefault();
-    document.getElementById('calory-needs').reset();
-    setNeeds(0);
-    setTargetValues({...0});
-  };
-
   return (
-    <div id="energy">
+    <Box className="energy">
       <h2>Calcul des dépenses energétiques</h2>
-      <form id="calory-needs" onSubmit={handleSubmit}>
-        <label htmlFor="height">Taille:</label>
-        <input
-          type="number"
-          name="height"
-          onChange={handleChange}
-        />
-        <label htmlFor="weight">Poids:</label>
-        <input
-          type="number"
-          name="weight"
-          onChange={handleChange}
-        />
-        <label htmlFor="age">Age:</label>
-        <input
-          type="number"
-          name="age"
-          onChange={handleChange}
-        />
-        <h4>Dépenses énergétique de base</h4>
-        <div className="activity">
-          {activityCheckBoxes.map(elem =>
-            <div key={elem.name.toString()}>
-              <input
-                className="activityLevel"
-                type="checkbox"
-                name="activityLevel"
-                id={elem.name}
-                value={elem.value}
-                defaultChecked={false}
-                onChange={handleChange}
-              />
-              <label htmlFor={elem.name}>{elem.name}</label>
-            </div>
-          )}
-        </div>
-        <h4>Dépenses selon la pratique sportive</h4>
-        <div className="sport">
-          {sportCheckBoxes.map(elem =>
-            <div key={elem.name.toString()}>
-              <input
-                className="sportLevel"
-                type="checkbox"
-                name="sportLevel"
-                id={elem.name}
-                value={elem.value}
-                defaultChecked={false}
-                onChange={handleChange}
-              />
-              <label htmlFor={elem.name}>{elem.name}</label>
-            </div>
-          )}
-        </div>
-        <div className="sport-time">
-          <label htmlFor="sportTime">Hours per week:</label>
-          <input
-            type="number"
-            name="sportTime"
-            onChange={handleChange}
-          />
-        </div>
-        <h4>Dépenses selon le genre</h4>
-        <div className="gender">
-          {genderCheckBoxes.map(elem =>
-            <div key={elem.name.toString()}>
-              <input
-                className="genderLevel"
-                type="checkbox"
-                name="genderLevel"
-                value={elem.value}
-                id={elem.name}
-                defaultChecked={false}
-                onChange={handleChange}
-              />
-              <label htmlFor={elem.name}>{elem.name}</label>
-            </div>
-          )}
-        </div>
-        <div className="calculation-result">Resultat : {needs}</div>
-        <div className="call-to-action">
-          <button className="button-calculate" type="submit" >Calculer</button>
-          <button className="button-clear" onClick={handleClear}>Initialiser</button>
-        </div>
-      </form>
-    </div>
+      <Card sx={{ padding: '2rem', margin: 'auto', width: '50%' }} component="form" onSubmit={handleSubmit} ref={formRef}>
+        <FormGroup id="energy-needs" sx={{ gap: '10px' }}>
+          <FormControl>
+            <InputLabel>Taille</InputLabel>
+            <OutlinedInput
+              type="number"
+              name="height"
+              label="Taille"
+              endAdornment={<InputAdornment position="end">cm</InputAdornment>}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <InputLabel>Poids</InputLabel>
+            <OutlinedInput
+              type="number"
+              name="weight"
+              label="Poids"
+              endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <InputLabel>Age</InputLabel>
+            <OutlinedInput
+              type="number"
+              name="age"
+              label="Age"
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel component="legend">Dépenses énergétique de base</FormLabel>
+            <RadioGroup row value={values.activityLevel} onChange={handleChange}>
+              {activityRadios.map(elem =>
+                <FormControlLabel
+                  key={elem.name}
+                  control={<Radio />}
+                  labelPlacement="start"
+                  name="activityLevel"
+                  label={elem.name}
+                  value={elem.value}
+                />
+              )}
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel component="legend">Dépenses selon la pratique sportive</FormLabel>
+            <RadioGroup row value={values.sportLevel} onChange={handleChange} >
+              {sportRadios.map(elem =>
+                <FormControlLabel key={elem.name}
+                  control={<Radio />}
+                  name="sportLevel"
+                  label={elem.name}
+                  value={elem.value}
+                  labelPlacement="start"
+                />
+              )}
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <InputLabel>Heures/Semaine</InputLabel>
+            <OutlinedInput
+              type="number"
+              name="sportTime"
+              label="Heures/Semaine "
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl >
+            <FormLabel component="legend">Dépenses selon le genre</FormLabel>
+            <RadioGroup row value={values.genderLevel} onChange={handleChange}>
+              {genderRadios.map(elem =>
+                <FormControlLabel
+                  key={elem.name}
+                  control={<Radio />}
+                  name="genderLevel"
+                  labelPlacement="start"
+                  label={elem.name}
+                  value={elem.value}
+                  defaultValue='female'
+                />
+              )}
+            </RadioGroup>
+          </FormControl>
+
+          <Paper className="calculation-result">Resultat : {needs}</Paper>
+          <Box className="call-to-action">
+            <Button className="button-calculate" type="submit" variant="outlined" color="success">Calculer</Button>
+            <Button className="button-clear" onClick={handleClear} variant="outlined" color="warning">Initialiser</Button>
+          </Box>
+        </FormGroup>
+      </Card>
+    </Box >
   );
 };
 
